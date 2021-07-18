cde9cc911dfe126459424bd734a839f0d3551e9c
yacyj22 <69840170+yacyj22@users.noreply.github.com>
2021-07-18 23:23:21 +0900

    Update mytrade2.py

--- mytrade2.py
+++ mytrade2.py
@@ -19,6 +19,17 @@ def rsi(ohlc: pandas.DataFrame, period: int = 14):
 
     return pandas.Series(100-(100/(1+RS)), name="RSI")
 
+def get_balance(ticker):
+    """잔고 조회"""
+    balances = upbit.get_balances()
+    for b in balances:
+        if b['currency'] == ticker:
+            if b['balance'] is not None:
+                return float(b['balance'])
+            else:
+                return 0
+    return 0
+
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
 coin = ""
